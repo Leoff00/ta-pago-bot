c32ce6c5151774e2b3b527bd13bff9d334deb517
Vitor Koch <vitorgabrielkoch@gmail.com>
2024-01-18 20:31:00 -0300

    feat: add database connection

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"log"
 
+	"github.com/leoff00/ta-pago-bot/database"
 	"github.com/leoff00/ta-pago-bot/pkg/bot"
 )
 
 func main() {
 	log.Default().Println("TA PAGO! The bot.")
-	bot.Start()
+	
+	db, err := database.Setup()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	bot.Start(db)
 }
