71c45da3fc6e06a1d53be6c90afa916a7568d9e3
Vitor Koch <vitorgabrielkoch@gmail.com>
2024-01-18 21:16:55 -0300

    feat: add display art

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/leoff00/ta-pago-bot/database"
 	"github.com/leoff00/ta-pago-bot/pkg/bot"
 )
 
+const displayArt = "\033[36m" + ` 
+$$$$$$$$\  $$$$$$\        $$$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  $$\ 
+\__$$  __|$$  __$$\       $$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$ |
+   $$ |   $$ /  $$ |      $$ |  $$ |$$ /  $$ |$$ /  \__|$$ /  $$ |$$ |
+   $$ |   $$$$$$$$ |      $$$$$$$  |$$$$$$$$ |$$ |$$$$\ $$ |  $$ |$$ |
+   $$ |   $$  __$$ |      $$  ____/ $$  __$$ |$$ |\_$$ |$$ |  $$ |\__|
+   $$ |   $$ |  $$ |      $$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |    
+   $$ |   $$ |  $$ |      $$ |      $$ |  $$ |\$$$$$$  | $$$$$$  |$$\ 
+   \__|   \__|  \__|      \__|      \__|  \__| \______/  \______/ \__|
+                                 The bot.
+                                  ❚█══█❚
+` + "\033[0m"
+
 func main() {
-	log.Default().Println("TA PAGO! The bot.")
-	
+	fmt.Println(displayArt)
+
 	db, err := database.Setup()
 	if err != nil {
 		log.Fatal(err.Error())
